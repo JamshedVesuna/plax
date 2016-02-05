c61030ce0913aafeab48e5b79b7d9c052fd6eda7
JamshedVesuna <jamshed.vesuna@gmail.com>
2016-02-05 14:55:03 -0800

    Added Homebrew to raw_install

--- raw_install.sh
+++ raw_install.sh
@@ -1,5 +1,9 @@
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 brew update
 brew install ansible git
+
 mkdir -p ~/scripts
 git clone https://github.com/JamshedVesuna/plax ~/scripts/plax
-cd ~/scripts/plax; ansible-playbook -i inventory playbook.yml
+cd ~/scripts/plax; ansible-playbook -i inventory playbook.yml; cd -
