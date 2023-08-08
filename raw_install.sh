07c32d5d768b27347faf1cd9a87b104f54fb407e
Jamshed Vesuna <jamshed@jamsheds-mbp.lan>
2023-08-08 10:47:59 -0700

    Homebrew updates

--- raw_install.sh
+++ raw_install.sh
@@ -1,5 +1,7 @@
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/jamshed/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 brew update
 brew install ansible git
